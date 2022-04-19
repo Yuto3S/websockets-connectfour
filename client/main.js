6cf397dd8da5a850224c0ecf1eb6167bec821ddd
Hugues Marandeau <h.marandeau@gmail.com>
2022-04-19 17:41:47 +0200

    Configure WebSocket server address.

--- client/main.js
+++ client/main.js
@@ -1,12 +1,21 @@
 import { createBoard, playMove } from "./connect4.js"
 
+function getWebSocketServer() {
+    if (window.location.host === "yuto3s.github.io") {
+        return "wss://websockets-tutorial.herokuapp.com/";
+    } else if (window.location.host === "localhost:8000") {
+        return "ws://localhost:8001/";
+    } else {
+        throw new Error(`Unsupported host: ${window.location.host}`);
+    }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     // Initialize the UI.
     const board = document.querySelector(".board");
     createBoard(board);
 
-
-    const websocket = new WebSocket("ws://localhost:8001/");
+    const websocket = new WebSocket(getWebSocketServer());
 
     websocket.addEventListener("message", ({ data }) => {
       const event = JSON.parse(data);
