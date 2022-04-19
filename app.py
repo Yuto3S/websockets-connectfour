9091a631cb623db81c7cc641578b667340518690
Hugues Marandeau <h.marandeau@gmail.com>
2022-04-19 17:29:41 +0200

    Add configuration to have website running on GitHub / Heroku

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import asyncio
 import json
 import secrets
+import os
+import signal
 
 import websockets
 from connect4 import PLAYER1, PLAYER2, Connect4
@@ -129,79 +131,14 @@ async def handler(websocket):
         await start(websocket)
 
 
-""" PART 1
-async def handler(websocket):
-    ### Add the game logic
-    game = Connect4()
-    player = PLAYER1
-    async for message in websocket:
-        message = json.loads(message)
-
-        if message["type"] != "play":
-            event = {
-                "type": "error",
-                "message": "Client should only send 'play' events",
-            }
-        else:
-            try:
-                column = message["column"]
-                row = game.play(player, column)
-                player = PLAYER2 if (player == PLAYER1) else PLAYER1
-                event = {
-                    "type": "play",
-                    "player": player,
-                    "column": column,
-                    "row": row,
-                }
-            except RuntimeError as e:
-                event = {
-                    "type": "error",
-                    "message": f"Move is illegal: {e}",
-                }
-
-        await websocket.send(json.dumps(event))
-
-        if game.winner:
-            event = {
-                "type": "win",
-                "player": player,
-            }
-            await websocket.send(json.dumps(event))
-
-    ### Transmit from server to browser
-
-    # for player, column, row in [
-    #     (PLAYER1, 3, 0),
-    #     (PLAYER2, 3, 1),
-    #     (PLAYER1, 4, 0),
-    #     (PLAYER2, 4, 1),
-    #     (PLAYER1, 2, 0),
-    #     (PLAYER2, 1, 0),
-    #     (PLAYER1, 5, 0),
-    # ]:
-    #     event = {
-    #         "type": "play",
-    #         "player": player,
-    #         "column": column,
-    #         "row": row,
-    #     }
-    #     await websocket.send(json.dumps(event))
-    #     await asyncio.sleep(0.5)
-    #
-    # event = {
-    #     "type": "win",
-    #     "player": PLAYER1,
-    # }
-    # await websocket.send(json.dumps(event))
-
-    # async for message in websocket:
-    #     print(message)
-"""
-
-
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
