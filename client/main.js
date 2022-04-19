9041bffd9b7624c6c895c345d73e4c429ccda2da
Hugues Marandeau <h.marandeau@gmail.com>
2022-04-19 17:44:11 +0200

    amend

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { createBoard, playMove } from "./connect4.js"
 
 function getWebSocketServer() {
     if (window.location.host === "yuto3s.github.io") {
-        return "wss://websockets-tutorial.herokuapp.com/";
+        return "wss://yuto3s-websockets-connectfour.herokuapp.com";
     } else if (window.location.host === "localhost:8000") {
         return "ws://localhost:8001/";
     } else {
