89094d0668c2613efb58dd07c6f8bd5288116332
Hugues Marandeau <h.marandeau@gmail.com>
2022-04-19 17:18:43 +0200

    Add functionally for 2 players on the same local network, with watchers. Part 2 - ROUTE & BROADCAST

--- app.py
+++ app.py
@@ -2,13 +2,137 @@
 
 import asyncio
 import json
+import secrets
 
 import websockets
 from connect4 import PLAYER1, PLAYER2, Connect4
 
+JOIN = {}
+WATCH = {}
+
+
+async def error(websocket, message):
+    event = {
+        "type": "error",
+        "message": message,
+    }
+    await websocket.send(json.dumps(event))
+
+
+async def start(websocket):
+    game = Connect4()
+    connected = {websocket}
+
+    join_key = secrets.token_urlsafe(12)
+    JOIN[join_key] = game, connected
+
+    watch_key = secrets.token_urlsafe(12)
+    WATCH[watch_key] = game, connected
+
+    try:
+        event = {
+            "type": "init",
+            "join": join_key,
+            "watch": watch_key,
+        }
+        await websocket.send(json.dumps(event))
+
+        print(f"First player started game {id(game)}.")
+        await play(websocket, game, PLAYER1, connected)
+    finally:
+        del JOIN[join_key]
+        del WATCH[watch_key]
+
+
+async def join(websocket, join_key):
+    try:
+        game, connected = JOIN[join_key]
+    except KeyError:
+        await error(websocket, "Game not found.")
+        return
+
+    connected.add(websocket)
+    try:
+        await replay(websocket, game)
+        print(f"Second player joined the game {id(game)}.")
+        await play(websocket, game, PLAYER2, connected)
+    finally:
+        connected.remove(websocket)
+
+
+async def watch(websocket, watch_key):
+    try:
+        game, connected = WATCH[watch_key]
+    except KeyError:
+        await error(websocket, "Game not found.")
+        return
+
+    connected.add(websocket)
+    try:
+        await replay(websocket, game)
+        print(f"Spectactor joined the game {id(game)}.")
+        await websocket.wait_closed()
+    finally:
+        connected.remove(websocket)
+
+
+async def play(websocket, game, player, connected):
+    async for message in websocket:
+        message = json.loads(message)
+        try:
+            column = message["column"]
+            row = game.play(player, column)
+            event = {
+                "type": "play",
+                "player": player,
+                "column": column,
+                "row": row,
+            }
+            for connected_player in connected:
+                await connected_player.send(json.dumps(event))
+        except RuntimeError as e:
+            event = {
+                "type": "error",
+                "message": f"Move is illegal: {e}",
+            }
+            await websocket.send(json.dumps(event))
+
+        if game.winner:
+            event = {
+                "type": "win",
+                "player": player,
+            }
+            for connected_player in connected:
+                await connected_player.send(json.dumps(event))
+
+
+async def replay(websocket, game):
+    for player, column, row in game.moves:
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
+
 
 async def handler(websocket):
-    """Add the game logic"""
+    message = await websocket.recv()
+    event = json.loads(message)
+    assert event["type"] == "init"
+
+    if "join" in event:
+        await join(websocket, event["join"])
+    elif "watch" in event:
+        await watch(websocket, event["watch"])
+    else:
+        await start(websocket)
+
+
+""" PART 1
+async def handler(websocket):
+    ### Add the game logic
     game = Connect4()
     player = PLAYER1
     async for message in websocket:
@@ -45,7 +169,7 @@ async def handler(websocket):
             }
             await websocket.send(json.dumps(event))
 
-    """ Transmit from server to browser
+    ### Transmit from server to browser
 
     # for player, column, row in [
     #     (PLAYER1, 3, 0),
@@ -69,10 +193,11 @@ async def handler(websocket):
     #     "type": "win",
     #     "player": PLAYER1,
     # }
-    # await websocket.send(json.dumps(event)) """
+    # await websocket.send(json.dumps(event))
 
     # async for message in websocket:
     #     print(message)
+"""
 
 
 async def main():
--- client/main.js
+++ client/main.js
@@ -13,11 +13,31 @@ window.addEventListener("DOMContentLoaded", () => {
       console.log(event);
     });
 
+    initGame(websocket);
     receiveMoves(board, websocket);
     sendMoves(board, websocket);
 });
 
+function initGame(websocket) {
+    websocket.addEventListener("open", () => {
+        const params = new URLSearchParams(window.location.search);
+        let event = { type: "init" };
+        if (params.has("join")){
+            event.join = params.get("join");
+        } else if (params.has("watch")) {
+            event.watch = params.get("watch");
+        }
+        console.log(event);
+        websocket.send(JSON.stringify(event));
+    });
+}
+
 function sendMoves(board, websocket) {
+    const params = new URLSearchParams(window.location.search);
+    if(params.has("watch")) {
+        return;
+    }
+
     board.addEventListener("click", ({ target }) => {
         const column = target.dataset.column;
         if (column == undefined) {
@@ -37,6 +57,10 @@ function receiveMoves(board, websocket)  {
     websocket.addEventListener("message", ({ data }) => {
         const event = JSON.parse(data);
         switch (event.type) {
+            case "init":
+                document.querySelector(".join").href = "?join=" + event.join;
+                document.querySelector(".watch").href = "?watch=" + event.watch;
+                break;
             case "play":
                 playMove(board, event.player, event.column, event.row);
                 break;
